f1c151962cfbdeac8ec02b11e06668c0e5ac84f6
Shailendra Sharma <shailendra.sharma@gmail.com>
2022-11-21 08:53:49 +0530

    small fixes in user role

--- blazectl/namespace/user.sh
+++ blazectl/namespace/user.sh
@@ -1,6 +1,8 @@
+set -x
+
 eksctl create iamidentitymapping \
     --cluster "${EKS_CLUSTER_NAME}" \
     --region="${AWS_DEFAULT_REGION}" \
     --arn arn:aws:iam::"${AWS_ACCOUNT_ID}":user/"$USER" \
-    --group "$ROLE_GROUP" \
+    --group "${ROLE_GROUP}" \
     --no-duplicate-arns
