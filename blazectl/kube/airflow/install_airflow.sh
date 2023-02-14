ab541d9f618a26c77afdc8adfd4eaa742316f82b
Shailendra Sharma <shailendra.sharma@gmail.com>
2023-02-14 21:48:14 +0530

    updated project version, docker image version, and added install airflow script

---
+++ blazectl/kube/airflow/install_airflow.sh
@@ -0,0 +1,2 @@
+envsubst < airflow_git_sync_secret.yaml | kubectl apply -n control-apps-airflow -f -
+envsubst < airflow_overwrite.yaml | helm upgrade --install airflow apache-airflow/airflow --namespace control-apps-airflow --create-namespace -f -
